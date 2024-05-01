a4ba7361d92afd28113112a6c12e0f1db31d9269
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2024-05-01 18:43:45 +0300

    Update play.py

--- YMusic/plugins/sounds/play.py
+++ YMusic/plugins/sounds/play.py
@@ -112,7 +112,7 @@ async def _aPlay(_, message):
                 add_to_queue(chat_id, title[:19], duration, songlink, link)
                 finish_time = time.time()
                 total_time_taken = str(int(finish_time - start_time)) + "s"
-                await m.edit(f"Tera gaana play kar rha hu aaja vc\n\nSongName:- [{title[:19]}]({link})\nDuration:- {duration}\nTime taken to play:- {total_time_taken}\nowner:-[⏤͟͞ 𓃬 𝗞ِ𝗮َِ𝗿ِٕ𝗮َِ𝗿◝ .](yttps://t.me/k_r_9k)\nطلب : {requester}", disable_web_page_preview=True)
+                await m.edit(f"Tera gaana play kar rha hu aaja vc\n\nSongName:- [{title[:19]}]({link})\nDuration:- {duration}\nTime taken to play:- {total_time_taken}\nowner:-[⏤͟͞ 𓃬 𝗞ِ𝗮َِ𝗿ِٕ𝗮َِ𝗿◝ .](yttps://t.me/k_r_9k)", disable_web_page_preview=True)
 
 
 @app.on_message((filters.command(PLAY_COMMAND, PREFIX) | filters.command(PLAY_COMMAND, RPREFIX)) & SUDOERS)
