a69ca7a2a4b37f5a4000f702cc856d7dfbf39791
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2024-05-02 22:54:17 +0300

    Update songs.py

--- YMusic/plugins/sounds/songs.py
+++ YMusic/plugins/sounds/songs.py
@@ -4,9 +4,8 @@ import aiohttp
 import yt_dlp
 from pyrogram import Client, filters
 from youtube_search import YoutubeSearch
-from m8n import app
-from m8n.config import BOT_USERNAME
-from m8n.utils.filters import command
+from YMusic import app
+from YMusic.utils.filters import command
 
 
 def time_to_seconds(time):
@@ -52,7 +51,7 @@ def song(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**‹ تم التحميل بواسطة ›** @{BOT_USERNAME}"
+        rep = f"**‹ تم التحميل بواسطة ›** {message.from_user.mention}"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
