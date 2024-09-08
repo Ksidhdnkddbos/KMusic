14d58c56d3c23dda3aed5ff23b5ff9fec4ffe57a
Ksidhdnkddbos <163964022+Ksidhdnkddbos@users.noreply.github.com>
2024-09-08 10:29:27 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,23 +1,16 @@
-import os
+from os import getenv
+from dotenv import load_dotenv
+load_dotenv()
 
-
-API_ID: int = int(os.getenv("API_ID")) or int()
-
-API_HASH: str = os.getenv("API_HASH") or str("")
-
-SESSION_STRING: str = os.getenv("SESSION_STRING") or str("")
-
-OWNER_ID: list[int] = [int(os.getenv("OWNER_ID"))] or []
-
-LOG_GROUP_ID: int = int(os.getenv("LOG_GROUP_ID")) or int()
-
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+SESSION_STRING = getenv("SESSION_STRING")
+OWNER_ID = int(getenv("OWNER_ID", 0))
 COOK_PATH = getenv("COOK_PATH")
 
-PREFIX: str = str("")
-
-RPREFIX: str = str("$")
-
+PREFIX = "."
+RPREFIX = "$"
 
 # No Need To Edit Below This
 
-LOG_FILE_NAME: str = "YMusic.txt"
+LOG_FILE_NAME = "YMusic.txt"
